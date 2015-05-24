13dd6aff86edbac532c84a9c9c0dc78cd75c04f2
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-23 23:25:56 -0300

    Inserindo dados na tabela.

---
+++ Aula01/InserindoDados.sql
@@ -0,0 +1,3 @@
+insert into ALUNO(codigo, nome, idade) values(1, 'Tiago Pires Guimarães', 26);
+insert into ALUNO(codigo, nome, idade) values(2, 'Suely Souza Santos Guimarães', 35);
+insert into ALUNO(codigo, nome, idade) values(3, 'Adna Souza Santos', 12);
