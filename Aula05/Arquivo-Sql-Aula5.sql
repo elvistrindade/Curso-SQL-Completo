6d850b6e8ed7367683963901ecea9fd4de5af994
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 12:52:39 -0300

    Inserindo mais dados na tabela TurmaItem.

--- Aula05/Arquivo-Sql-Aula5.sql
+++ Aula05/Arquivo-Sql-Aula5.sql
@@ -1,9 +1,13 @@
-select * from ALUNO;
-select * from TURMA;
+/*select * from ALUNO;
+select * from TURMA;*
 select * from TURMAITEM;
 
 /*--=============================================--
 insert into TURMA values(1, 'Turma 6ª B', 2013);
 
 --=============================================--
-insert into TURMAITEM values(1,3);*/
+insert into TURMAITEM values(1,3);
+
+--=============================================--*/
+insert into TURMAITEM values(1, 6);
+insert into TURMAITEM values(1, 2);*/
