bd4460f3a5a24cf88b181be0792c573653d9905c
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 11:03:32 -0300

    Criando a tabela TurmaItem, a qual possui uma chave primária composta.

--- Aula04/Arquivo-Sql-Aula4.sql
+++ Aula04/Arquivo-Sql-Aula4.sql
@@ -1,7 +1,15 @@
-create table TURMA (
+/*create table TURMA (
     codigo int not null,
     nome varchar(20) not null,
     ano int not null,
     
     primary key(codigo)
+);*/
+
+--========================================================--
+create table TurmaItem (
+    codigo_turma int not null,
+    codigo_aluno int not null,
+    
+    primary key(codigo_turma, codigo_aluno)    
 );
