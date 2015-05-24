e23ea70ffcb62dc6a786625cf35410c06a01355e
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 11:11:20 -0300

    Alterando a tabela TurmaItem para adicionar uma chave estrangeira, relacionando esta com a tabela Turma.

--- Aula04/Arquivo-Sql-Aula4.sql
+++ Aula04/Arquivo-Sql-Aula4.sql
@@ -7,9 +7,15 @@
 );*/
 
 --========================================================--
-create table TurmaItem (
+/*create table TurmaItem (
     codigo_turma int not null,
     codigo_aluno int not null,
     
     primary key(codigo_turma, codigo_aluno)    
-);
+);*/
+
+--========================================================--
+alter table TURMAITEM add constraint FK_TURMAITEM_TURMA
+    foreign key (CODIGO_TURMA) references TURMA (CODIGO)
+    on update CASCADE 
+    on delete NO ACTION;
