57353414a002d0203b550bb819c2a375cd74fa0b
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 13:42:10 -0300

    Inserindo registros na tabela TurmaItem.

--- Aula06/Arquivo-Sql-Aula6.sql
+++ Aula06/Arquivo-Sql-Aula6.sql
@@ -3,6 +3,11 @@ select * from TURMA;
 select * from TURMAITEM;*/
 
 --=============================================--
-insert into TURMA values(2, 'Turma 6ª A', 2013);
+/*insert into TURMA values(2, 'Turma 6ª A', 2013);
 insert into TURMA values(3, 'Turma 7ª A', 2013);
+commit;*/
+
+--=============================================--
+insert into TURMAITEM values(2, 4);
+insert into TURMAITEM values(3, 1);
 commit;
