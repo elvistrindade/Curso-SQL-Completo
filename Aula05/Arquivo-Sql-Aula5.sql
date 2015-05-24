fbf113ad548cb3700572a2d9749eec1dc9da3c80
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 12:48:55 -0300

    Buscando informações da tabela TurmaItem.

--- Aula05/Arquivo-Sql-Aula5.sql
+++ Aula05/Arquivo-Sql-Aula5.sql
@@ -1,8 +1,9 @@
-/*select * from ALUNO;
+select * from ALUNO;
 select * from TURMA;
+select * from TURMAITEM;
+
+/*--=============================================--
+insert into TURMA values(1, 'Turma 6ª B', 2013);
 
 --=============================================--
-insert into TURMA values(1, 'Turma 6ª B', 2013);*/
-
---=============================================--
-insert into TURMAITEM values(1,3);
+insert into TURMAITEM values(1,3);*/
