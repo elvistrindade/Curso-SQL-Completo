ee14a8744df76665ac15ed1de4b4b3c69b2fbfb1
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-23 23:43:05 -0300

    Atualizando mais de uma tabela em um único comando.

--- Aula02/ArquivoSQL.sql
+++ Aula02/ArquivoSQL.sql
@@ -1,1 +1,6 @@
-update ALUNO set idade = 11 where codigo = 3;
+update aluno set idade = 11 where codigo = 3;
+
+update aluno
+    set idade = 12,
+        nome = 'Adna Suellen Souza Santos'
+    where codigo = 3;
