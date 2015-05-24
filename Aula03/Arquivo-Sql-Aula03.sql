bebe00505a9ab877ecd2f67ea9cd9cf202ae2e3e
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 10:48:26 -0300

    Pesquisando os registros existentes na tabela Aluno.

--- Aula03/Arquivo-Sql-Aula03.sql
+++ Aula03/Arquivo-Sql-Aula03.sql
@@ -1,5 +1,6 @@
-insert into ALUNO values(4, 'Fulano de Tal da Silva', 39);
+/*insert into ALUNO values(4, 'Fulano de Tal da Silva', 39);
 insert into ALUNO values(5, 'Ciclano Silveira Sales', 28);
-insert into ALUNO values(6, 'Deltrano Vieira Silva', 27);
+insert into ALUNO values(6, 'Deltrano Vieira Silva', 27);*/
 
 --========================================================--
+select * from ALUNO;
