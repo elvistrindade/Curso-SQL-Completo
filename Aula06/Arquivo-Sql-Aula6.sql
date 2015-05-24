3bd9e482aefd12758a815f28b48caa51bf0c7346
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 13:53:43 -0300

    Buscando relação dos alunos que não estão cadastrados em uma turma com o Exists.

--- Aula06/Arquivo-Sql-Aula6.sql
+++ Aula06/Arquivo-Sql-Aula6.sql
@@ -1,20 +1,25 @@
 --select * from ALUNO;
 --select * from TURMA;
 --select * from TURMAITEM;
+select * from ALUNO A
+where not exists (select 1
+                from TURMAITEM TI
+                where TI.CODIGO_ALUNO = A.CODIGO
+                );
 
---=============================================--
+--================================================================--
 /*insert into TURMA values(2, 'Turma 6ª A', 2013);
 insert into TURMA values(3, 'Turma 7ª A', 2013);
 commit;*/
 
---=============================================--
+--================================================================--
 /*insert into TURMAITEM values(2, 4);
 insert into TURMAITEM values(3, 1);
 commit;*/
---=============================================--
-insert into ALUNO values(7, 'Hadassa Lídia', 19);
+--================================================================--
+/*insert into ALUNO values(7, 'Hadassa Lídia', 19);
 insert into ALUNO values(8, 'Maria Clara', 26);
 insert into ALUNO values(9, 'Anderson Freire', 38);
 insert into ALUNO values(10, 'Antônio Paulo', 45);
 insert into ALUNO values(11, 'João Júnior', 17);
-commit;
+commit;*/
