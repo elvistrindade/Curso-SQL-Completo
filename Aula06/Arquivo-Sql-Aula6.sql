59ed6fa127f88706f4689179ae631812a0a03dfb
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 13:38:37 -0300

    Inserindo registros na tabela Turma.

---
+++ Aula06/Arquivo-Sql-Aula6.sql
@@ -0,0 +1,8 @@
+/*select * from ALUNO;
+select * from TURMA;
+select * from TURMAITEM;*/
+
+--=============================================--
+insert into TURMA values(2, 'Turma 6ª A', 2013);
+insert into TURMA values(3, 'Turma 7ª A', 2013);
+commit;
