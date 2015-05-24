809b76964d1d9e564fd0cb3bc794b1199128c9bc
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 13:46:31 -0300

    Inserindo registros na tabela Aluno.

--- Aula06/Arquivo-Sql-Aula6.sql
+++ Aula06/Arquivo-Sql-Aula6.sql
@@ -1,6 +1,6 @@
-/*select * from ALUNO;
-select * from TURMA;
-select * from TURMAITEM;*/
+--select * from ALUNO;
+--select * from TURMA;
+--select * from TURMAITEM;
 
 --=============================================--
 /*insert into TURMA values(2, 'Turma 6ª A', 2013);
@@ -8,6 +8,13 @@ insert into TURMA values(3, 'Turma 7ª A', 2013);
 commit;*/
 
 --=============================================--
-insert into TURMAITEM values(2, 4);
+/*insert into TURMAITEM values(2, 4);
 insert into TURMAITEM values(3, 1);
+commit;*/
+--=============================================--
+insert into ALUNO values(7, 'Hadassa Lídia', 19);
+insert into ALUNO values(8, 'Maria Clara', 26);
+insert into ALUNO values(9, 'Anderson Freire', 38);
+insert into ALUNO values(10, 'Antônio Paulo', 45);
+insert into ALUNO values(11, 'João Júnior', 17);
 commit;
