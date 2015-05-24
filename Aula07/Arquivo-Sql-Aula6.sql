0a3863ba02fe38d3e4c132196dd511fa4cc90297
Tiago Pires Guimarães <tiagopgu.pires@gmail.com>
2015-05-24 14:12:20 -0300

    Usando union novamente para entender a diferença entre este e o union all.

--- Aula07/Arquivo-Sql-Aula6.sql
+++ Aula07/Arquivo-Sql-Aula6.sql
@@ -2,9 +2,19 @@ select A.CODIGO, A.NOME, A.IDADE, 'Com Turma' as DESCRICAO from ALUNO A
 where exists(select 1 from TURMAITEM TI
             where TI.CODIGO_ALUNO = A.CODIGO)
             
-union
+union all
             
 select A.CODIGO, A.NOME, A.IDADE, 'Sem Turma' as DESCRICAO from ALUNO A
 where not exists(select 1 from TURMAITEM TI
             where TI.CODIGO_ALUNO = A.CODIGO)
+            
+union
+
+select 10 as CODIGO,
+    'Astronildo Bertolo' as NOME,
+    50 as IDADE,
+    'Não consta' as DESCRICAO
+from ALUNO A
+where exists(select 1 from TURMAITEM TI where TI.CODIGO_ALUNO = A.CODIGO)
+
 order by 4, 2;
